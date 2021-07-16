d04504c89f822f588d5823848b357284c134f4df
waleed alqallaf <waleedalqalaf2@gmail.com>
2021-07-16 23:28:26 +0300

    defaults for the three textboxes

--- ui/beat_map_editor/ViewportContainer.gd
+++ ui/beat_map_editor/ViewportContainer.gd
@@ -1,6 +1,6 @@
 extends Viewport
 
-onready var texture_rect = $"./TextureRect"
+#onready var texture_rect = $"./TextureRect"
 
 func _ready():
 	#texture_rect.texture =  texture_rect.get_viewport().get_texture()
